a77c7913a9e145174e832aeb12209b9f5b372fd8
lalahaha323 <https://github.com/lalahaha323>
2019-05-05 22:26:11 +0800

    启动自动代理

--- src/main/java/chapter_04/concert/Audience.java
+++ src/main/java/chapter_04/concert/Audience.java
@@ -1,27 +1,19 @@
 package chapter_04.concert;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 @Aspect//该注解表明Audience不仅仅是一个POJO，还是一个切面
 public class Audience {
     @Pointcut("execution(* chapter_04.concert.Performance.perform(..))")
     public void performance() {}
-    @Before("performance()")
-    public void silenceCellPhones() {
-        System.out.println("Silencing cell phones");
-    }
-    @Before("performance()")
-    public void takeSeats() {
-        System.out.println("Taking seats");
-    }
-    @AfterReturning("performance()")
-    public void applause() {
-        System.out.println("CLAP CLAP CLAP!!!");
-    }
-    @AfterThrowing("performance()")
-    public void demandRefund() {
-        System.out.println("Demanding a refund");
+    @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint jp) {
+        try {
+            System.out.println("Silencing cell phones");
+            System.out.println("Taking seats");
+            jp.proceed();
+            System.out.println("CLAP CLAP!!!");
+        } catch(Throwable e) {
+            System.out.println("Demanding a refund");
+        }
     }
 }
