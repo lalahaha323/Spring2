9c12fa1e6bfb428be564f28aa0b44cb4675347fc
lalahaha323 <https://github.com/lalahaha323>
2019-05-05 21:55:33 +0800

    创建切面优化

--- src/main/java/chapter_04/concert/Audience.java
+++ src/main/java/chapter_04/concert/Audience.java
@@ -3,22 +3,24 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-//这个类的四个方法都使用注解来定义切面的具体行为
+import org.aspectj.lang.annotation.Pointcut;
 @Aspect//该注解表明Audience不仅仅是一个POJO，还是一个切面
 public class Audience {
-    @Before("execution(* chapter_04.concert.Performance.perform(..))")
+    @Pointcut("execution(* chapter_04.concert.Performance.perform(..))")
+    public void performance() {}
+    @Before("performance()")
     public void silenceCellPhones() {
         System.out.println("Silencing cell phones");
     }
-    @Before("execution(* chapter_04.concert.Performance.perform(..))")
+    @Before("performance()")
     public void takeSeats() {
         System.out.println("Taking seats");
     }
-    @AfterReturning("execution(* chapter_04.concert.Performance.perform(..))")
+    @AfterReturning("performance()")
     public void applause() {
         System.out.println("CLAP CLAP CLAP!!!");
     }
-    @AfterThrowing("execution(* chapter_04.concert.Performance.perform(..))")
+    @AfterThrowing("performance()")
     public void demandRefund() {
         System.out.println("Demanding a refund");
     }
